5945b8c4a990748285797e391175a34b3d8a8827
not ervin <notminin@pm.me>
2018-08-28 21:27:42 +0100

    noo I have to use jqueryyyy

--- js/logoLoader.js
+++ js/logoLoader.js
@@ -1,30 +1,36 @@
-var logoNum = getRandomInt(5);
-var colCombinations = [['#FFF', '#D81B60', '#FFF'], ['#FFF', '#000', '#FFF'], ['#000', '#FFF', '#000'], ['#D81B60', '#FFF', '#D81B60'], ['#161616', '#eee', '#161616']];
-var combNum = getRandomInt(colCombinations.length + 1);
-var selComb = colCombinations[combNum];
+$(document).ready(function () {
+    let logoNum = getRandomInt(5);
+    let colCombinations = [['#FFFFFF', '#D81B60', '#FFFFFF'],
+                           ['#FFFFFF', '#000000', '#FFFFFF'],
+                           ['#000000', '#FFFFFF', '#000000'],
+                           ['#D81B60', '#FFFFFF', '#D81B60'],
+                           ['#161616', '#EEEEEE', '#161616']];
+    let combNum = getRandomInt(colCombinations.length + 1);
+    let selComb = colCombinations[combNum];
 
-$('.logoContainer').each(() => {
-    $(this).svg({
-        onLoad: () => {
-            var svg = $(this).svg('get');
-            svg.load(`svg/logo/CL${logoNum}.svg`, { addTo: true, changeSize: true });
-        },
-        settings: {}
+    $('.logoContainer').each(function () {
+        $(this).svg({
+            onLoad: function () {
+                let svg = $(this).svg('get');
+                svg.load(`svg/logo/CL${logoNum}.svg`, { addTo: true, changeSize: true });
+            },
+            settings: {}
+        });
     });
+
+    setTimeout(function () {
+        $('.logoContainer').find('.outline').css({ 'stroke': selComb[0] });
+
+        if ($('.logoContainer svg').hasClass('polyback')) {
+            $('.logoContainer').find('.background').css({ 'fill': selComb[1] });
+        } else {
+            $('.logoContainer').find('pattern').find('.background').css({ 'fill': selComb[1] });
+        }
+
+        if ($('.logoContainer').find('pattern').find('.fill').hasClass('circle')) {
+            $('.logoContainer').find('pattern').find('.fill').css({ 'fill': selComb[2] });
+        } else {
+            $('.logoContainer').find('pattern').find('.fill').css({ 'stroke': selComb[2] });
+        }
+    }, 50);
 });
-
-setTimeout(() => {
-    $('.logoContainer').find('.outline').css({ 'stroke': selComb[0] });
-
-    if ($('.logoContainer svg').hasClass('polyback')) {
-        $('.logoContainer').find('.background').css({ 'fill': selComb[1] });
-    } else {
-        $('.logoContainer').find('pattern').find('.background').css({ 'fill': selComb[1] });
-    }
-
-    if ($('.logoContainer').find('pattern').find('.fill').hasClass('circle')) {
-        $('.logoContainer').find('pattern').find('.fill').css({ 'fill': selComb[2] });
-    } else {
-        $('.logoContainer').find('pattern').find('.fill').css({ 'stroke': selComb[2] });
-    }
-}, 50);
