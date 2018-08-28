8187534c36614b474e2c1e254066407be8d7b98e
not ervin <notminin@pm.me>
2018-08-28 18:44:56 +0100

    optimize and hop don did fuc up

--- js/logoLoader.js
+++ js/logoLoader.js
@@ -1,34 +1,37 @@
-$(document).ready(function () {
-    var logoNum = getRandomInt(5);
-    var colCombinations = [['#FFF', '#D81B60', '#FFF'], ['#FFF', '#000', '#FFF'], ['#000', '#FFF', '#000'], ['#D81B60', '#FFF', '#D81B60'], ['#161616', '#eee', '#161616']];
-    var combNum = getRandomInt(colCombinations.length + 1);
-    var selComb = colCombinations[combNum];
+const colCombinations = [['#FFFFFF', '#D81B60', '#FFFFFF'],
+                         ['#FFFFFF', '#000000', '#FFFFFF'],
+                         ['#000000', '#FFFFFF', '#000000'],
+                         ['#D81B60', '#FFFFFF', '#D81B60'],
+                         ['#161616', '#EEEEEE', '#161616']];
 
-    $('.logoContainer').each(function () {
-        $(this).svg({
-            onLoad: function () {
-                var svg = $(this).svg('get');
-                svg.load(`svg/logo/CL${logoNum}.svg`, { addTo: true, changeSize: true });
-            },
-            settings: {}
-        }
-        );
-    });
+let logoNum = getRandomInt(5);
+let selComb = colCombinations[getRandomInt(colCombinations.length + 1)];
 
-    setTimeout(function () {
-        $('.logoContainer').find('.outline').css({ 'stroke': selComb[0] });
-
-        if ($('.logoContainer svg').hasClass('polyback')) {
-            $('.logoContainer').find('.background').css({ 'fill': selComb[1] });
-        } else {
-            $('.logoContainer').find('pattern').find('.background').css({ 'fill': selComb[1] });
-        }
-
-        if ($('.logoContainer').find('pattern').find('.fill').hasClass('circle')) {
-            $('.logoContainer').find('pattern').find('.fill').css({ 'fill': selComb[2] });
-        } else {
-            $('.logoContainer').find('pattern').find('.fill').css({ 'stroke': selComb[2] });
-        }
-
-    }, 50);
+$('.logoContainer').each(() => {
+    $(this).svg({
+        onLoad: () => {
+            let svg = $(this).svg('get');
+            svg.load(`svg/logo/CL${logoNum}.svg`, { addTo: true, changeSize: true });
+        },
+        settings: {}
+    }
+    );
 });
+
+setTimeout(() => {
+    let logoContainer = $('.logoContainer');
+    let pattern = logoContainer.find('pattern');
+    logoContainer.find('.outline').css({ 'stroke': selComb[0] });
+
+    if ($('.logoContainer svg').hasClass('polyback')) {
+        logoContainer.find('.background').css({ 'fill': selComb[1] });
+    } else {
+        pattern.find('.background').css({ 'fill': selComb[1] });
+    }
+
+    if (pattern.find('.fill').hasClass('circle')) {
+        pattern.find('.fill').css({ 'fill': selComb[2] });
+    } else {
+        pattern.find('.fill').css({ 'stroke': selComb[2] });
+    }
+}, 50);
