d696dc58ca3df2e943348f6535a3ff1c5f0cbe0e
GhostlyDilemma <ghostly.dilemma@gmail.com>
2019-03-30 00:54:46 +0100

    Fixes Issues with nav bar on smaller monitors

--- js/index.js
+++ js/index.js
@@ -26,73 +26,7 @@ const observer = new IntersectionObserver(entries => {
       if (entry.isIntersecting) {
         // If they are an Idle Callback is called to not process the changes on the Main Thread
         window.requestIdleCallback(() => {
-          const curSelection = document.querySelector(".currentSection");
-          // Selecting the Currently Selected Info Holder (poss. val: main, secondary, tertiary)
-          const visElement = curSelection.getAttribute("visibleelement");
-          // Gets Text of Currently Selected Info Holder
-          const selectedElementText = curSelection.querySelector(
-            `.${visElement}`
-          ).innerText;
-          const selectedElementScroll = title[entry.target.id];
-          // If the Currently Selected Text and the Title from the currently overscrolling object are the same (if they are, do nothing)
-          if (selectedElementText != selectedElementScroll) {
-            curSelection.onclick = () => {
-              document
-                .getElementById(entry.target.id)
-                .scrollIntoView({ behavior: "smooth" });
-            };
-            // If the Overscrolling Object is the header, switch to the main Info Holder & Animate the Home Button to go Up
-            if (selectedElementScroll == "Customa") {
-              let animateHome = anime({
-                targets: ".homeBar",
-                translateX: 45.5,
-                easing: "easeOutElastic(.1, 2)"
-              });
-              const goMain = anime({
-                targets: curSelection,
-                translateY: 30,
-                complete: curSelection.setAttribute("visibleelement", "main"),
-                easing: "easeOutElastic(.1, 2)"
-              });
-            } else {
-              // Animate the Home Button to go Up
-              let animateHome = anime({
-                targets: ".homeBar",
-                translateX: document
-                  .querySelector(`nav a.${entry.target.id}`)
-                  .getAttribute("transform"),
-                easing: "easeOutElastic(.1, 2)"
-              });
-              let curSelected;
-              let translationIndex;
-              // If Currently Selected Info Holder is main or tertiary, go to secondary
-              switch (visElement) {
-                case "main":
-                case "tertiary":
-                  curSelected = "secondary";
-                  translationIndex = -1;
-                  break;
-                // If Currently Selected Info Holder is secondary, go to tertiary
-                case "secondary":
-                  curSelected = "tertiary";
-                  translationIndex = -30;
-                  break;
-              }
-              curSelection.querySelector(
-                `.${curSelected}`
-              ).innerHTML = selectedElementScroll;
-
-              const goSelection = anime({
-                targets: curSelection,
-                translateY: translationIndex,
-                complete: curSelection.setAttribute(
-                  "visibleelement",
-                  `${curSelected}`
-                ),
-                easing: "easeOutElastic(.1, 2)"
-              });
-            }
-          }
+          observerFunction(entry.target.id);
         });
       }
     },
@@ -123,6 +57,86 @@ function scrollToGroup(links) {
     });
     link.removeAttribute("href");
   });
+}
+
+function observerFunction(id) {
+  const curSelection = document.querySelector(".currentSection");
+  // Selecting the Currently Selected Info Holder (poss. val: main, secondary, tertiary)
+  const visElement = curSelection.getAttribute("visibleelement");
+  // Gets Text of Currently Selected Info Holder
+  const selectedElementText = curSelection.querySelector(`.${visElement}`)
+    .innerText;
+  const selectedElementScroll = title[id];
+  // If the Currently Selected Text and the Title from the currently overscrolling object are the same (if they are, do nothing)
+  if (selectedElementText != selectedElementScroll) {
+    curSelection.onclick = () => {
+      document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+    };
+    // If the Overscrolling Object is the header, switch to the main Info Holder & Animate the Home Button to go Up
+    if (selectedElementScroll == "Customa") {
+      let animationVal = positionLink(
+        document.querySelector(`nav a.currentSection`)
+      );
+      let animateHome = anime({
+        targets: ".homeBar",
+        translateX: animationVal[0],
+        easing: "easeOutElastic(.1, 2)",
+        complete: document
+          .querySelector(`nav a.currentSection`)
+          .setAttribute("over", "currentSection")
+      });
+      const goMain = anime({
+        targets: curSelection,
+        translateY: 30,
+        complete: curSelection.setAttribute("visibleelement", "main"),
+        easing: "easeOutElastic(.1, 2)"
+      });
+    } else {
+      // Animate the Home Button to go Up
+      let animationVal = positionLink(document.querySelector(`nav a.${id}`));
+      let animateBar = anime({
+        targets: ".homeBar",
+        translateX: animationVal[0],
+        easing: "easeOutElastic(.1, 2)"
+      });
+      let animationButton = anime({
+        targets: ".homeBar a",
+        width: animationVal[1],
+        easing: "easeInOutQuad",
+        complete: document
+          .querySelector(`nav .homeBar a`)
+          .setAttribute("over", id)
+      });
+      let curSelected;
+      let translationIndex;
+      // If Currently Selected Info Holder is main or tertiary, go to secondary
+      switch (visElement) {
+        case "main":
+        case "tertiary":
+          curSelected = "secondary";
+          translationIndex = -1;
+          break;
+        // If Currently Selected Info Holder is secondary, go to tertiary
+        case "secondary":
+          curSelected = "tertiary";
+          translationIndex = -30;
+          break;
+      }
+      curSelection.querySelector(
+        `.${curSelected}`
+      ).innerHTML = selectedElementScroll;
+      const goSelection = anime({
+        targets: curSelection,
+        translateY: translationIndex,
+        complete: curSelection.setAttribute("visibleelement", `${curSelected}`),
+        easing: "easeOutElastic(.1, 2)"
+      });
+    }
+  }
+}
+
+function positionLink(link) {
+  return [link.getBoundingClientRect().left - 30, link.offsetWidth + 60];
 }
 
 document.querySelector(".settings .settingsHandle").onclick = () => {
